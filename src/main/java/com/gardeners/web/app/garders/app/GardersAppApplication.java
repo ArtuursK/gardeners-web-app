d8ad40d578d32407394b81591a13a3a0442e6c05
Arturs Kempelis <arturs.kempelis_1@rtu.lv>
2023-02-14 14:00:07 +0200

    HB-002 Spring project setup

---
+++ src/main/java/com/gardeners/web/app/garders/app/GardersAppApplication.java
@@ -0,0 +1,13 @@
+package com.gardeners.web.app.garders.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardersAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gardeners/web/app/garders/app/GardersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gardeners.web.app.garders.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardersAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
